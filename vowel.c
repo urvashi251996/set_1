961ff3aef4408c263b09b02099124965c7b9bf0e
urvashi251996 <44240855+urvashi251996@users.noreply.github.com>
2019-03-14 14:14:29 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+scanf("%c",&ch);
+if(ch=='A'||ch=='E'||ch=="I"||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+printf("entered character is vowel");
+else
+printf("entered character is consonant");
