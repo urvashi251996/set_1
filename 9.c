63fe1f1f2a5a70e77dfa1b4b11dee4a8c8f12753
urvashi251996 <44240855+urvashi251996@users.noreply.github.com>
2019-05-22 11:31:55 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int sum=0,i,N,K;
+    int arr[5];
+    scanf("%d %d",&n,&k);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<=K;i++)
+    {
+    sum+=i;
+}
+printf("%d",sum);
+}
